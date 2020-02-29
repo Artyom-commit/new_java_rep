15984cb5423a65906d537da4360f0f5831c17402
Artyom-commit <longcycle@mail.ru>
2020-02-29 15:28:58 +0300

    First programm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	  System.out.println("Hello world");
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите ваше сообщение: ");
+        String message = sc.nextLine();
+
+        System.out.println("Вы напечатали: " + message);
     }
 }
